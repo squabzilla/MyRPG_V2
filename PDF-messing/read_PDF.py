1f3aa141a2f81da5f25f4242ac02b4432a339eec
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-28 21:58:25 +0000

    Jun 28, 2024, 3:58 PM

--- PDF-messing/read_PDF.py
+++ PDF-messing/read_PDF.py
@@ -9,9 +9,26 @@ page = reader.pages[170]
 text_default = page.extract_text()
 text_layout = page.extract_text(extraction_mode="layout")
 text_plain = page.extract_text(extraction_mode="plain")
-print(text_default)
+#print(text_default)
 #print(text_layout)
 #print(text_plain)
+
+# extract only text oriented up
+#print(page.extract_text(0))
+
+# extract text oriented up and turned left
+#print(page.extract_text((0, 90)))
+
+# extract text in a fixed width format that closely adheres to the rendered
+# layout in the source pdf
 #print(page.extract_text(extraction_mode="layout"))
+
+# extract text preserving horizontal positioning without excess vertical
+# whitespace (removes blank and "whitespace only" lines)
 #print(page.extract_text(extraction_mode="layout", layout_mode_space_vertically=False))
-#print(page.extract_text(extraction_mode="layout", layout_mode_scale_weight=1.0))
+
+# adjust horizontal spacing
+print(page.extract_text(extraction_mode="layout", layout_mode_scale_weight=1.0))
+
+# exclude (default) or include (as shown below) text rotated w.r.t. the page
+#print(page.extract_text(extraction_mode="layout", layout_mode_strip_rotated=False))
