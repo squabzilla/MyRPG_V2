8ff4bdd6cd11686e03f1af5c4738ad5882c55fa7
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-28 22:34:10 +0000

    Jun 28, 2024, 4:34 PM

--- PDF-messing/pdfplumber_read_PDF.py
+++ PDF-messing/pdfplumber_read_PDF.py
@@ -1,6 +1,12 @@
 import pdfplumber
 pdf = pdfplumber.open('SRD_CC_v5.1.pdf')
 page = pdf.pages[171]
+#bounding_box = (x0, top, x1, bottom)
+# bounding box, which should be expressed as 4-tuple with the values (x0, top, x1, bottom)
+#bottom = page.crop((0, 0.8 * float(page.height), page.width, page.height))
+#bottom_left = bottom.crop((0, 0, 0.5 * float(bottom.width), bottom.height))
+#page = page.crop((x0, top, x1, bottom), relative=False, strict=True)
+page = page.crop((0, 0, 0.5 * float(page.width), page.height), relative=False, strict=True)
 text = page.extract_text()
 print(text)
 pdf.close()
