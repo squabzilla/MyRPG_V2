681e45ef065e628612a182f5e1cc02c8b2df4fa2
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-28 22:01:10 +0000

    Jun 28, 2024, 4:01 PM

--- PDF-messing/read_PDF.py
+++ PDF-messing/read_PDF.py
@@ -5,7 +5,7 @@ from pypdf import PdfReader
 
 reader = PdfReader("SRD_CC_v5.1.pdf")
 number_of_pages = len(reader.pages)
-page = reader.pages[170]
+page = reader.pages[2]
 text_default = page.extract_text()
 text_layout = page.extract_text(extraction_mode="layout")
 text_plain = page.extract_text(extraction_mode="plain")
@@ -21,14 +21,14 @@ text_plain = page.extract_text(extraction_mode="plain")
 
 # extract text in a fixed width format that closely adheres to the rendered
 # layout in the source pdf
-#print(page.extract_text(extraction_mode="layout"))
+print(page.extract_text(extraction_mode="layout"))
 
 # extract text preserving horizontal positioning without excess vertical
 # whitespace (removes blank and "whitespace only" lines)
 #print(page.extract_text(extraction_mode="layout", layout_mode_space_vertically=False))
 
 # adjust horizontal spacing
-print(page.extract_text(extraction_mode="layout", layout_mode_scale_weight=1.0))
+#print(page.extract_text(extraction_mode="layout", layout_mode_scale_weight=1.0))
 
 # exclude (default) or include (as shown below) text rotated w.r.t. the page
 #print(page.extract_text(extraction_mode="layout", layout_mode_strip_rotated=False))
