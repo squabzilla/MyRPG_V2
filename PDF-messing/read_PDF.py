977f1fe47429d04cd5cf5a8ddaf81e12c70831ab
squabzilla <55065342+squabzilla@users.noreply.github.com>
2024-06-28 21:49:17 +0000

    Jun 28, 2024, 3:49 PM

--- PDF-messing/read_PDF.py
+++ PDF-messing/read_PDF.py
@@ -8,5 +8,6 @@ number_of_pages = len(reader.pages)
 page = reader.pages[170]
 text = page.extract_text()
 #print(text)
-#print(page.extract_text(extraction_mode="layout"))
-print(page.extract_text(extraction_mode="layout", layout_mode_space_vertically=False))
+print(page.extract_text(extraction_mode="layout"))
+#print(page.extract_text(extraction_mode="layout", layout_mode_space_vertically=False))
+#print(page.extract_text(extraction_mode="layout", layout_mode_scale_weight=1.0))
